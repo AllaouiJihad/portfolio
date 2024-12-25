6a5410f0fea193980bc8ebafc9a03d6f0da56c66
jihad <allaouijihad05@gmail.com>
2024-12-25 10:16:48 +0100

    portfolio

---
+++ src/constants/URLs.ts
@@ -0,0 +1,15 @@
+export const COMPANY_URL = 'https://www.elementica.io/'
+export const UDEMY_URL =
+  'https://www.udemy.com/course/javascript_full/?couponCode=KEEPLEARNING'
+export const IMT_URL = 'https://imt.academy/'
+export const UNIVERSITY_URL = '#'
+export const TELEGRAM_URL = 'http://t.me/Vlad_K15'
+export const LINKEDIN_URL =
+  'https://www.linkedin.com/in/vlad-kryvytchenko-62500621a/'
+export const GIT_HUB_URL = 'https://github.com/Ska1503/portfolio/tree/main/src'
+export const UP_WORK_URL =
+  'https://www.upwork.com/freelancers/~0119d7b73ab9152f03'
+export const ASX_URL = 'https://www.asx.ai/'
+export const TUTTIFY_URL = 'https://tuttify.io/'
+export const MAVEN_URL = 'https://www.maven11.com/'
+export const APP_URL = 'https://vladfrontend.pro'
